605eea8b2f3a20649d432319252af3fa74e35346
Jobe Range <rangejobe@gmail.com>
2018-07-21 23:31:52 -0700

    push this

---
+++ assets/javascript/profile.js
@@ -0,0 +1,37 @@
+
+// var about = $(".about");
+// var portfolio = $(".portfolio");
+// var blog = $(".blog");
+// var featured = $(".featured")
+
+
+
+
+// // $(document).ready(function() {
+// //     about.hide();
+// //     portfolio.hide();
+// //     blog.hide();
+// //     featured.hide();
+// // })
+
+// if ($(".tag-about").on("click")) {
+//     about.show();
+//     portfolio.hide();
+//     blog.hide();
+//     featured.hide();
+// } else if($(".tag-portfolio").on("click")) {
+//     portfolio.show();
+//     about.hide();
+//     blog.hide();
+//     featured.hide();
+// } else if ($(".tag-featured").on("click")) {
+//     featured.show();
+//     about.hide();
+//     blog.hide();
+//     portfolio.hide();
+// } else if ($(".tag-blog").on("click")) {
+//     blog.show();
+//     about.hide();
+//     portfolio.hide();
+//     featured.hide();
+// }
